2e5b58698440c07911607407592c6b73bd140db0
Jens Reimann <jens.reimann@ibh-systems.com>
2014-11-14 11:39:17 +0100

    initial import

---
+++ jetty-9.2.x/README.md
@@ -0,0 +1,3 @@
+
+* Be sure not to have whitespaces in your launch path, Jetty can't handle that
+* Start 'org.eclipse.jetty.osgi.boot' after all other bundles have started
---
+++ web1/src/de/dentrassi/osgiee/web1/TestServlet.java
@@ -0,0 +1,21 @@
+package de.dentrassi.osgiee.web1;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TestServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		resp.setContentType("text/plain");
+		resp.getWriter().println ( "Foo bar" );
+	}
+}
